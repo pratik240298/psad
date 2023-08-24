603b771aea3810b74993f7832a4b8e578a4cc63f
Admin <Admin@192.168.0.4>
2023-08-24 13:54:49 +0530

    cjc

---
+++ demonstration/src/demonstration/A.java
@@ -0,0 +1,19 @@
+package demonstration;
+
+import java.util.Scanner;
+public class A {
+
+
+	int a;
+	String name;
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter value of a");
+	int a=sc.nextInt();	
+	System.out.println("Enter a name");
+	String name=sc.nextLine();
+	}
+
+}
+
